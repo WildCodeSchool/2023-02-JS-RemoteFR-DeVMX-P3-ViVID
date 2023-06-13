5ba38298493a25e36d5da6c51a12285f9856ee9a
RudyR1 <rudy.rano972@gmail.com>
2023-06-13 23:16:19 +0200

    Front-createFooter_part1

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header/Header";
 import LeftNavBar from "../components/navBar/LeftNavBar";
+import Footer from "../components/Footer/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <LeftNavBar />
+      <Footer />
     </>
   );
 }
