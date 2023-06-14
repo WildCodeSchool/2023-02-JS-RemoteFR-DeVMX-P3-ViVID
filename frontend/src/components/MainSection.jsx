cca6c142a33807eb9e82c183bda1bec67daf8d2d
RudyR1 <rudy.rano972@gmail.com>
2023-06-14 13:43:54 +0200

    add logo, contact, copyright on footer

--- frontend/src/components/MainSection.jsx
+++ frontend/src/components/MainSection.jsx
@@ -1,9 +1,11 @@
 import Caroussel from "./videosDisplay/Caroussel";
 import "./mainSection.scss";
+import BasicSlider from "./HeroSlider";
 
 export default function MainSection() {
   return (
     <section className="body">
+      <BasicSlider />
       <Caroussel />
     </section>
   );
