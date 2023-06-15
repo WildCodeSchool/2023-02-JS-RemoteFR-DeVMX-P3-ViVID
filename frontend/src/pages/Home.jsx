493fe0da10f03a6b9219c8ab8869150dad0d7cd5
Yiroma <yiromaric@gmail.com>
2023-06-15 12:11:36 +0200

    creat modal connection and style

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header/Header";
 import LeftNavBar from "../components/navBar/LeftNavBar";
 import MainSection from "../components/MainSection";
 import Footer from "../components/Footer/Footer";
+
 import "./home.scss";
 
 export default function Home() {
