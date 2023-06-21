517e9014c4b4393d2b2d1db14e21932af156573f
Yiroma <yiromaric@gmail.com>
2023-06-21 11:35:00 +0200

    clean code

--- frontend/src/components/videosDisplay/HeroSlider.jsx
+++ frontend/src/components/videosDisplay/HeroSlider.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import previousImg from "../../assets/previous.png";
 import nextImg from "../../assets/next.png";
