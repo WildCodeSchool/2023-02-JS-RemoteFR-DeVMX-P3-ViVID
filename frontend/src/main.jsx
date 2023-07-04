7a90cef8c2222ec926ed465c8cb6cbeb1cfc8681
RudyR1 <rudy-rano_student2023@wilder.school>
2023-07-04 09:58:57 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { UserProvider } from "./contexts/UserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
