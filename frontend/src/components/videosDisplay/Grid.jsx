467a0cd28b82b44819c047c58618956e8c56ca14
Yiroma <yiromaric@gmail.com>
2023-07-24 10:22:03 +0200

    modify title in grid

--- frontend/src/components/videosDisplay/Grid.jsx
+++ frontend/src/components/videosDisplay/Grid.jsx
@@ -24,18 +24,41 @@ export default function Grid({ categoryId }) {
   }, []);
 
   return (
-    <div className="grid">
-      {videos &&
-        videos.map((vid) => (
-          <Link className="card" key={vid.id} to={`/videos/${vid.id}`}>
-            <img
-              src={`${import.meta.env.VITE_BACKEND_URL}${vid.thumbnail}`}
-              alt={vid.title}
-            />
-            <h2>{vid.title}</h2>
-          </Link>
-        ))}
-    </div>
+    <>
+      <h3 className={categoryId === 1 ? "showtext" : "hide"}>
+        Selection pour vous
+      </h3>
+      <h3 className={categoryId === 2 ? "showtext" : "hide"}>
+        Selection pour vous dans Ciel & Nuages
+      </h3>
+      <h3 className={categoryId === 3 ? "showtext" : "hide"}>
+        Selection pour vous dans Montagnes
+      </h3>
+      <h3 className={categoryId === 4 ? "showtext" : "hide"}>
+        Selection pour vous dans Forêt
+      </h3>
+      <h3 className={categoryId === 5 ? "showtext" : "hide"}>
+        Selection pour vous dans Mer & Océans
+      </h3>
+      <h3 className={categoryId === 6 ? "showtext" : "hide"}>
+        Selection pour vous dans Urbain
+      </h3>
+      <h3 className={categoryId === 7 ? "showtext" : "hide"}>
+        Selection pour vous dans Champ
+      </h3>
+      <div className="grid">
+        {videos &&
+          videos.map((vid) => (
+            <Link className="card" key={vid.id} to={`/videos/${vid.id}`}>
+              <img
+                src={`${import.meta.env.VITE_BACKEND_URL}${vid.thumbnail}`}
+                alt={vid.title}
+              />
+              <h2>{vid.title}</h2>
+            </Link>
+          ))}
+      </div>
+    </>
   );
 }
 
